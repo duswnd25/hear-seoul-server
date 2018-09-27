429e1010795063047c1d994b0c9867d409fb2dc0
duswnd25 <duswnd25@gmail.com>
2018-09-27 15:15:08 +0900

    수정

--- src/routes/api/main.js
+++ src/routes/api/main.js
@@ -8,40 +8,66 @@ Router.get("/time", function (req, res) {
 });
 
 Router.get("/suggestion-upload", function (req, routerRes) {
-    let options = {
-        url: 'https://apis.sktelecom.com/v1/baas/data/Suggestion',
-        method: 'POST',
-        headers: {
-            "Accept": "application/json",
-            "TDCProjectKey": process.env.BAAS_API_KEY,
-            "Content-Type": "application/json;charset=utf-8"
-        },
-        body: JSON.stringify({
-            "id": req.query.id,
-            "suggest": req.query.suggest
-        })
-    };
-
-    request(options, function (err, res, body) {
-        return routerRes.status(200).json(JSON.parse(body));
-    });
+    if (req.query.key === process.env.CLIENT_API_KEY) {
+        let options = {
+            url: 'https://apis.sktelecom.com/v1/baas/data/Suggestion',
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "TDCProjectKey": process.env.BAAS_API_KEY,
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                "id": req.query.id,
+                "suggest": req.query.suggest
+            })
+        };
+        request(options, function (err, res, body) {
+            return routerRes.status(200).json(JSON.parse(body));
+        });
+    } else {
+        return routerRes.status(200).json({result: "KEY 값을 확인 해 주세요"});
+    }
 });
 
-Router.get("/request", function (req, routerRes) {
-    let options = {
-        url: 'https://apis.sktelecom.com/v1/baas/data/Spot',
-        method: 'GET',
-        headers: {
-            "Accept": "application/json",
-            "TDCProjectKey": process.env.BAAS_API_KEY,
-            "Content-Type": "application/json;charset=utf-8"
-        }
-    };
-
-    request(options, function (err, res, body) {
-        return routerRes.status(200).json(JSON.parse(body));
-    });
+Router.get("/get-spot-data", function (req, routerRes) {
+    if (req.query.key === process.env.CLIENT_API_KEY) {
+        let options = {
+            url: 'https://apis.sktelecom.com/v1/baas/data/Spot',
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                "TDCProjectKey": process.env.BAAS_API_KEY,
+                "Content-Type": "application/json;charset=utf-8"
+            }
+        };
+        request(options, function (err, res, body) {
+            return routerRes.status(200).json(JSON.parse(body));
+        });
+    }
+    else {
+        return routerRes.status(200).json({result: "KEY 값을 확인 해 주세요"});
+    }
 });
 
+Router.get("/delete-image", function (req, resRouter) {
+    if (req.query.key === process.env.CLIENT_API_KEY) {
+        let options = {
+            url: 'https://apis.sktelecom.com/v1/baas/data/Test/' + req.query.id,
+            method: 'DELETE',
+            headers: {
+                "Accept": "application/json",
+                "TDCProjectKey": "dec67ee7-874b-41fd-9f8f-de39003337d9",
+                "Content-Type": "application/json;charset=utf-8"
+            }
+        };
+        request(options, function (err, res, body) {
+            return resRouter.status(200).json(JSON.parse(body));
+        });
+    }
+    else {
+        return resRouter.status(200).json({result: "KEY 값을 확인 해 주세요"});
+    }
+});
 
 module.exports = Router;
